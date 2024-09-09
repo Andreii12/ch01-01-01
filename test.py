22f28c314b6eb4317b25ddb46d674c4789179741
Andrii Osiiuk <73231932+Andreii12@users.noreply.github.com>
2024-09-09 12:23:15 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ start = date(2020, 1, 1)
 today = date.today()
 delta = today - start
 
+
+
 if (delta.days < 101):
-    print("Today is day {}".format(delta.days))
+     print("Today is day {}".format(delta.days))
 else:
     print('100 Days of Code sprint has ended')
